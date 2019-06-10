d2767c40d6e458262f4fef0a1dd8e0a7e13b9038
Kaicheng Hu <kaichenghu7@gmail.com>
2019-06-10 18:34:29 -0500

    more html/css and start js

---
+++ mycode/JS/Facebook.js
@@ -0,0 +1,20 @@
+var obj = {
+	username: "Kaicheng",
+	password: "HoldHey!",
+};
+
+var database = [obj];
+var newsfeed = [
+	{
+		username: "Kaicheng",
+		timeline: "HoldHey!",
+	},
+	{
+		username: "James",
+		timeline: "creepy",
+	},
+	{
+		username: "Alice",
+		timeline: "blocklol",
+	}
+];
